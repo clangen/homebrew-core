e17c6a56dba3558134c0c25863d3c34afbc46406
casey langen <casey.langen@gmail.com>
2023-01-07 12:14:20 -0800

    Slight tweaks to cmake commands, improve tests, add comments.

--- Formula/musikcube.rb
+++ Formula/musikcube.rb
@@ -1,8 +1,8 @@
 class Musikcube < Formula
   desc "Terminal-based audio engine, library, player and server"
   homepage "https://musikcube.com"
-  url "https://github.com/clangen/musikcube/archive/0.99.3.tar.gz"
-  sha256 "584c8225caad46fde83cdd520ef7d4a6633471de9693651f46d520040e9fb51d"
+  url "https://github.com/clangen/musikcube/archive/0.99.4.tar.gz"
+  sha256 "dd8e74c8c8460250c0da325941db2df0f7dcca77f2c68a0295ab21e81f8dd8a4"
   license "BSD-3-Clause"
   head "https://github.com/clangen/musikcube.git", branch: "master"
 
@@ -24,17 +24,19 @@ class Musikcube < Formula
   depends_on "taglib"
 
   def install
-    args = std_cmake_args + %w[
-      -DCMAKE_BUILD_TYPE=Release
-    ]
-    system "cmake", ".", *args
+    system "cmake", ".", *std_cmake_args
     system "make"
+    # running `cmake .` again updates cached cmake variables to include resources
+    # generated during the `make` phase; this ensures things like dynamic plugins,
+    # locales, and themes are included in the install manifest.
     system "cmake", "."
     system "make", "install"
   end
 
   test do
     system "musikcubed", "--start"
+    # if there is no lockfile then the daemon was unable to start properly
+    system "head", "/tmp/musikcubed.lock"
     system "musikcubed", "--stop"
   end
 end
